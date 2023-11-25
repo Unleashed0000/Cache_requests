a6b4659b7604f1d4eed3c49b3110d4704f89bc7a
mrmron <antondotnet@gmail.com>
2023-11-25 13:45:22 +0100

    add db

---
+++ app/database.py
@@ -0,0 +1,31 @@
+import redis
+import json
+from config import redis_host, redis_port
+
+
+class RedisCache:
+    def __init__(self, host='localhost', port=6379, db=0, decode_responses=True):
+        self.redis_client = redis.StrictRedis(host=host, port=port, db=db, decode_responses=decode_responses)
+
+    def get_from_cache(self, key):
+        cached_response = self.redis_client.get(key)
+        return json.loads(cached_response) if cached_response else None
+
+    def set_to_cache(self, key, value, expiration_time=3600):
+        self.redis_client.setex(key, expiration_time, json.dumps(value))
+
+    def get(self, key):
+        value =  self.redis_client.get(key)
+        return [] if value == "null" else json.loads(value)
+
+
+    def set(self, key, value):
+        # Сериализуем список в строку JSON перед сохранением в Redis
+        serialized_value = json.dumps(value)
+        self.redis_client.set(key, serialized_value)
+    def clear(self):
+        self.redis_client.flushdb()
+
+
+# Создание объекта RedisCache
+redis_cache = RedisCache()
