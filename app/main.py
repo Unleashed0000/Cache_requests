2da1f76fd2114f8f733d562037c4f982969a58b8
mrmron <antondotnet@gmail.com>
2023-11-25 10:22:47 +0100

    uvicorn run

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from routers.router import router as api_router
-
+import uvicorn
 
 app = FastAPI()
 
@@ -10,3 +10,7 @@ app.include_router(api_router)
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, log_level="info")
