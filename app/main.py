65950682fd11ff0c1626adbaecf41fcb71954914
Unleashed0000 <filinov.n@yandex.ru>
2023-11-26 13:28:48 +0300

    comments

--- app/main.py
+++ app/main.py
@@ -7,6 +7,9 @@ app = FastAPI()
 app.include_router(api_router)
 
 if __name__ == "__main__":
+    # local run
     uvicorn.run("main:app", log_level="info", reload=True)
+    # from remote server
     # uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info", reload=True)
+    # from command line remote server
     # uvicorn main:app --port 5000 --host '0.0.0.0' --reload
